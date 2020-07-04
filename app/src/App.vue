bc9a1f37872866478faf79335a3b95bb31853b90
Armeet Jatyani <xarmeetx@gmail.com>
2020-07-04 15:51:46 -0700

    initial commit

---
+++ app/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+
+import Todos from './components/Todos'
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Take a shower',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Brush teeth',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Eat breakfast',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Wake up',
+          completed: true
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/Todos.vue
@@ -0,0 +1,19 @@
+<template>
+    <div> 
+        <h1>To-Dos</h1>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <h3>{{todo.title}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Todos",
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
