ef7dcd4ff54fe820c5549b11a6ef3191c90ba328
Armeet Jatyani <xarmeetx@gmail.com>
2020-07-04 17:33:27 -0700

    added router and header on all pages

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,72 +1,43 @@
 <template>
   <div id="app">
-    <Header />
-    <AddTodo v-on:add-todo="addTodo"/>
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    <div id="nav">
+      <Header />
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-
-import Todos from './components/Todos'
 import Header from './components/layout/Header'
-import AddTodo from './components/AddTodo'
-import axios from 'axios'
-
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    Todos, Header, AddTodo
-  },
-  data() {
-    return {
-      todos: []
-    }
-  },
-  methods: {
-    deleteTodo (id) {
-      axios.delete(`https://jsonplaceholder.typicode.com/todos/{$id}`)
-      .then(() => this.todos = this.todos.filter(todo => todo.id !== id))
-      .catch(err => console.log(err))
-    },
-    addTodo (newTodo) {
-      const {title,completed} = newTodo;
-      axios.post('https://jsonplaceholder.typicode.com/todos', {
-        title,
-        completed
-      })
-      .then(res => this.todos = [...this.todos, res.data])
-      .catch(err => console.log(err))
-    }
-
-  },
-  created() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
-      .then(res => this.todos = res.data)
-      .catch(err => console.log(err))
+    Header
   }
 }
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
-  .btn:hover {
-    background: #666;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
