ad80467cee7c80ad6313116c4c43d1054db931ea
Armeet Jatyani <xarmeetx@gmail.com>
2020-07-04 17:37:56 -0700

    built to /dist

---
+++ app/dist/js/about.71a19cba.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("✔ v0.0.0")]),n("p",[t._v("A working to do list!")])])}],l=n("2877"),o={},r=Object(l["a"])(o,s,a,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=about.71a19cba.js.map
